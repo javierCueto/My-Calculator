c3ed10df06c63236f4cc636e9530af59222cbda8
javierCueto <javelie@outlook.com>
2020-06-25 20:39:20 -0600

    image and enable dark mode

--- My Calculator/View/ContentView.swift
+++ My Calculator/View/ContentView.swift
@@ -12,7 +12,7 @@ enum CalculatorButton: String{
     case zero, one, two, three, four, five, six, seven, eight, nine
     case equals, plus, minus, multiply, divide
     case decimal
-    case ac, plusMinus, percent
+    case ac, plusMinus, percent, delete
     
     var title: String {
         switch self {
@@ -26,14 +26,15 @@ enum CalculatorButton: String{
         case .seven : return "7"
         case .eight : return "8"
         case .nine : return "9"
-        case .plus : return "+"
-        case .minus : return "-"
-        case .multiply : return "x"
-        case .plusMinus : return "+/-"
+        case .plus : return "plus"
+        case .minus : return "minus"
+        case .multiply : return "multiply"
+        case .plusMinus : return "plus.slash.minus"
         case .percent : return "%"
-        case .equals : return "="
+        case .equals : return "equal"
         case .decimal : return "."
-        case .divide : return "/"
+        case .divide : return "divide"
+        case .delete : return "delete.left.fill"
         default:
             return "AC"
             
@@ -43,13 +44,49 @@ enum CalculatorButton: String{
     var backgroundColor: Color {
         switch self{
         case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .decimal:
-            return Color(.darkGray)
+            return Color("ColorDigits")
         case .ac, .plusMinus, .percent:
-            return Color(.lightGray)
+            return Color.blue
+        case .equals:
+            return Color.white
         default:
             return .orange
         }
     }
+    
+    
+    var arrayColor:[Color]{
+        switch self{
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .decimal:
+            return [Color("ColorButtons"),Color("ColorButtons2"),Color("ColorButtons2"),Color("ColorButtons2")]
+        case .ac, .plusMinus, .percent:
+            return [Color("ColorButtons"),Color("ColorButtons2"),Color("ColorButtons2"),Color("ColorButtons2")]
+        case .equals:
+            return [Color("ColorOrange1"),.orange]
+        default:
+            return [Color("ColorButtons"),Color("ColorButtons2"),Color("ColorButtons2"),Color("ColorButtons2")]
+        }
+    }
+    
+    
+    var shadownColor: Color{
+         switch self{
+         case .equals:
+             return Color("ColorShadownOrange")
+         default:
+             return Color("ColorShadown")
+         }
+     }
+    
+    var isImage: Bool{
+        switch self {
+        case .delete,.plus, .minus, .multiply, .plusMinus, .equals, .divide:
+            return true
+        default:
+            return false
+        }
+    }
+    
 }
 
 struct ContentView: View {
@@ -58,10 +95,10 @@ struct ContentView: View {
         [.ac, .seven, .four , .one, .zero],
         [.plusMinus , .eight, .five, .two, .decimal],
         [.percent , .nine, .six, .three, .equals],
-        [.divide , .multiply, .minus, .plus]
+        [.delete, .divide , .multiply, .minus, .plus]
     ]
     
-    
+   
     var body: some View {
         
         GeometryReader { geometry in
@@ -77,23 +114,26 @@ struct ContentView: View {
                             ForEach(row, id: \.self){ button in
                                 
                                 Button(action: {
-                                    
+                                    print(button.title)
                                 }){
+                                    ContentView2(button: button, geometryWidth: geometry.size.width)
                                     
-                                    Text(button.title)
+                                    /*Image(systemName: "multiply")
+                         
+                                         Text(button.title)
+                                 
+                                   
                                         .font(.title)
+                                        .foregroundColor(button.backgroundColor)
                                         .frame(width: (geometry.size.width/5), height: self.buttonHeight(button: button, height: geometry.size.width)  )
                                       
-                                        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorButtons"),Color.white,Color.white,Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing) )
-                                              
-                                        
-                                        //.border(Color.purple, width: 5, cornerRadius: 20)
+                                        .background(LinearGradient(gradient: Gradient(colors: button.arrayColor ), startPoint: .topLeading, endPoint: .bottomTrailing) )
                                        .cornerRadius((geometry.size.width/5))
                                         .overlay(Capsule().stroke(Color("ColorShadown"), lineWidth: 0.4))
                                         .padding(.horizontal, (geometry.size.width/5) / 10)
                                         .padding(.vertical, (geometry.size.width/5) / 10)
-                                        .shadow(color: Color("ColorShadown"), radius: 7, x: 10, y: 10)
-                                    
+                                        .shadow(color: button.shadownColor, radius: 7, x: 10, y: 10)
+                                    */
                                 }
                                 
                             }
@@ -110,26 +150,72 @@ struct ContentView: View {
             
             
         }
+        .background(Color("ColorBackground"))
+        .edgesIgnoringSafeArea(.all)
         
     }
     
     
     func buttonHeight(button: CalculatorButton, height: CGFloat) -> CGFloat{
-        if button == .plus{
-            
-            return (((height/5 )) * 2) + ((height/5 ) / 5)
-        }
+//        if button == .plus{
+//
+//            return (((height/5 )) * 2) + ((height/5 ) / 5)
+//        }
         return (height/5)
     }
-    
-    func  buttonIsPlus(button: CalculatorButton) -> Bool{
-        if button == .plus{
-            return true
+}
+
+struct ContentView2: View {
+  
+    let button: CalculatorButton
+    let geometryWidth: CGFloat
+
+    @ViewBuilder
+    var body: some View {
+        if button.isImage {
+            
+            
+            Image(systemName: button.title)
+                   .font(.title)
+                   .foregroundColor(button.backgroundColor)
+                   .frame(width: (geometryWidth/5), height: self.buttonHeight(button: button, height: geometryWidth)  )
+                 
+                   .background(LinearGradient(gradient: Gradient(colors: button.arrayColor ), startPoint: .topLeading, endPoint: .bottomTrailing) )
+                  .cornerRadius((geometryWidth/5))
+                   .overlay(Capsule().stroke(Color("ColorShadown"), lineWidth: 0.2))
+                   .padding(.horizontal, (geometryWidth/5) / 10)
+                   .padding(.vertical, (geometryWidth/5) / 10)
+                   .shadow(color: button.shadownColor, radius: 7, x: 6, y: 8)
+        }else{
+            
+            
+              Text(button.title)
+                   .font(.title)
+                   .foregroundColor(button.backgroundColor)
+                   .frame(width: (geometryWidth/5), height: self.buttonHeight(button: button, height: geometryWidth)  )
+                 
+                   .background(LinearGradient(gradient: Gradient(colors: button.arrayColor ), startPoint: .topLeading, endPoint: .bottomTrailing) )
+                  .cornerRadius((geometryWidth/5))
+                   .overlay(Capsule().stroke(Color("ColorShadown"), lineWidth: 0.2))
+                   .padding(.horizontal, (geometryWidth/5) / 10)
+                   .padding(.vertical, (geometryWidth/5) / 10)
+                   .shadow(color: button.shadownColor, radius: 7, x: 6, y: 8)
         }
-        return false
+        
     }
     
+    
+      
+        func buttonHeight(button: CalculatorButton, height: CGFloat) -> CGFloat{
+    //        if button == .plus{
+    //
+    //            return (((height/5 )) * 2) + ((height/5 ) / 5)
+    //        }
+            return (height/5)
+        }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
